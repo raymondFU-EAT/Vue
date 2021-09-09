4d2f537a6cdcf3364d4a70edf7cdec93a31147a8
Raymond BBF <ketchup942@gmail.com>
2021-09-09 12:55:10 +0800

    firstVue
    description

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'soft',
+            image:'./assets/images/socks_green.jpg',
+            url:'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3'
+        }
+    }
+
+})
